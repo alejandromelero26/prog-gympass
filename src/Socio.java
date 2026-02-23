1776ddbad52357f4abba419682e71f5f2b7683fc
Alejandro <amelevel@myuax.com>
2026-02-24 00:07:41 +0100

    clase socio

---
+++ src/Socio.java
@@ -0,0 +1,19 @@
+public class Socio {
+    private final int numSocio;
+    private final String nombre;
+
+    public Socio(int numSocio, String nombre) {
+        this.numSocio = numSocio;
+        this.nombre = nombre;
+    }
+    public int getNumSocio() {
+        return numSocio;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String toString() {
+        return "[" + numSocio + "] " + nombre;
+    }
+
+}
